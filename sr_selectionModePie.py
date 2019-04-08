5c80a3f383f6e9e015055830e2fa96b8c53570d9
Aurorah Harmony <aurorahHarmony@gmail.com>
2019-04-08 14:12:11 +1200

    Added better naming to View modes

--- sr_selectionModePie.py
+++ sr_selectionModePie.py
@@ -49,12 +49,12 @@ class Pie_MT_SelectionMode(Menu):
         pie = layout.menu_pie()
         pie.operator("mesh.select_mode", text="Vertex", icon='VERTEXSEL').type = 'VERT'
         pie.operator("mesh.select_mode", text="Face", icon='FACESEL').type = 'FACE'
-        pie.operator("rendered.mode")
+        pie.operator("rendered.mode", text="Rendered")
         pie.operator("mesh.select_mode", text="Edge", icon='EDGESEL').type = 'EDGE'
         pie.operator("object.mode_set", text="Object", icon='OBJECT_DATAMODE').mode = 'OBJECT'
         pie.operator("object.mode_set", text="Edit", icon='EDITMODE_HLT').mode = 'EDIT'
-        pie.operator("wire.mode")
-        pie.operator("solid.mode")
+        pie.operator("wire.mode", text="Wireframe")
+        pie.operator("solid.mode", text="Solid")
 
 
 
@@ -91,6 +91,6 @@ bl_info = {
     "author": "Aurorah - Scriptronix Studios",
     "version": (1, 0, 0),
     "blender": (2, 80, 0),
-    "description": "a more descriptive text of what your addon does",
+    "description": "A Pie menu that allows you to select your selection and view modes",
     "category": "User Interface"
 }
